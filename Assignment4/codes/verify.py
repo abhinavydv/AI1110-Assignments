0332baac378f50db2dd0a2d45933e25434e6b708
AbhinavYdv <abhinavyadavdev@gmail.com>
2022-04-23 00:19:42 +0530

    Updated verify.py

--- Assignment4/codes/verify.py
+++ Assignment4/codes/verify.py
@@ -7,17 +7,9 @@ students = np.array([1]*135 + [0]*65)
 
 # perform the experiment multiple times (say 10000 times) to get probability
 t = 10000
-pr_x_eq_0 = pr_x_eq_1 = 0
+choice = np.random.choice(students, size=t)
+unique, count = np.unique(choice, return_counts=True)
+zero_one = dict(zip(unique, count))
 
-for i in range(t):
-    c = np.random.choice(students)    # choose a student randomly
-    if c == 0:
-        pr_x_eq_0 += 1
-    elif c == 1:
-        pr_x_eq_1 += 1
-
-pr_x_eq_1 /= t
-pr_x_eq_0 /= t
-
-print("Pr(X=0) = ", pr_x_eq_0)
-print("Pr(X=1) = ", pr_x_eq_1)
+print("Pr(X = Like) = ", zero_one[1]/t)
+print("Pr(X = Dislike) = ", zero_one[0]/t)
